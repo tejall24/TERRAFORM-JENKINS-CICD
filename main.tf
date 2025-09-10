caacecf4b3446dbc8be61165db0d25b7aa9b0f5a
TEJAL AVINASH MOGAL <tejalmogal56@gmail.com>
2025-09-10 18:05:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "tejal" {
-  ami                    = "ami-0abcdef1234567890"   # wrap AMI in quotes
+  ami                    = ""ami-0c02fb55956c7d316"   # wrap AMI in quotes
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
