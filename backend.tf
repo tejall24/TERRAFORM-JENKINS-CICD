d64aedd196fa8623714d4ea4b6e878bdaaf8a58f
TEJAL AVINASH MOGAL <tejalmogal56@gmail.com>
2025-09-10 17:39:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "project24"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
